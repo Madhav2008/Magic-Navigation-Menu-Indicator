9e883dcc0232c27bf70ec0af483e84e62b05298b
arora <aroramadhav65@gmail.com>
2022-05-31 14:23:32 +0530

    Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const list = document.querySelectorAll('.list');
-        function activeLine() {
-            list.forEach((item) =>
-                item.classList.remove('active')
-            );
-            this.classList.add('active');
-        }
-        list.forEach((item) => 
-            item.addEventListener('click', activeLink)
-        );
+function activeLine() {
+    list.forEach((item) =>
+        item.classList.remove('active')
+    );
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink)
+);
